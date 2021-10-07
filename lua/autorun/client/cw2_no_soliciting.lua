cdc8e45f81aefff06069f1459344ce13e1b5dea8
Brandon Sturgeon <brandon@brandonsturgeon.com>
2021-10-07 13:53:34 -0700

    Now prevents solicitation through April 21, 2125

--- lua/autorun/client/cw2_no_soliciting.lua
+++ lua/autorun/client/cw2_no_soliciting.lua
@@ -1,4 +1,4 @@
 if not CustomizableWeaponry then return end
 
-file.Write( CustomizableWeaponry.baseFolder .. "/iv_message_time.txt", tostring( os.time() * 2 ) )
+file.Write( CustomizableWeaponry.baseFolder .. "/iv_message_time.txt", tostring( os.time() * 3 ) )
 hook.Remove( "InitPostEntity", "CW20_IVMessage" )
